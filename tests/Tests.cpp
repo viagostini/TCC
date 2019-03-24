a553742db792b252db57197cf906b61c3f3585b3
viagostini <vinicius.agostini19@gmail.com>
2019-03-23 23:20:45 -0300

    incluindo Table Algorithm em Tests

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -3,6 +3,7 @@
 #include "../src/edge.cpp"
 #include "../src/node.cpp"
 #include "../src/tree.cpp"
+#include "../src/TableAlgorithm.cpp"
 
 int default_graph_size = 7;
 vector<Edge> default_graph_edges = {
@@ -14,11 +15,39 @@ vector<Edge> default_graph_edges = {
     Edge(3, 6)
 };
 
+/* *********************************************************
+ *                                                         *      
+ *                      Edge Tests                         *     
+ *                                                         *    
+ * *********************************************************/
+
 TEST (EdgeTest, SimpleTest) {
     Edge edge(4, 5);
     ASSERT_EQ(4, edge.from());
     ASSERT_EQ(5, edge.to());
 }
+
+/* *********************************************************
+ *                                                         *      
+ *                      Node Tests                         *     
+ *                                                         *    
+ * *********************************************************/
+
+TEST (NodeTest, SimpleTest) {
+    Node node(2);
+    Node another_node(1);
+    ASSERT_EQ(2, node.label());
+    ASSERT_EQ(true, node.children().empty());
+    node.add_child(another_node);
+    ASSERT_EQ(1, node.children().size());
+    ASSERT_EQ(1, node.children()[0].label());
+}
+
+/* *********************************************************
+ *                                                         *      
+ *                      Tree Tests                         *     
+ *                                                         *    
+ * *********************************************************/
 
 TEST (TreeTest, SizeTest) {
     Tree tree(default_graph_size, default_graph_edges);
@@ -32,6 +61,22 @@ TEST (TreeTest, DFSTestSimple) {
     for (int i = 0; i < out.size(); i++) {
         EXPECT_EQ(dfs[i], out[i]);
     }
+}
+
+/* *********************************************************
+ *                                                         *      
+ *                  Table Algorithm Tests                  *     
+ *                                                         *    
+ * *********************************************************/
+
+TEST (TableAlgorithmTest, SimpleTest) {
+    Tree tree(default_graph_size, default_graph_edges);
+    TableAlgorithm table(tree);
+    ASSERT_EQ(0, table.query(0, 0));
+    ASSERT_EQ(0, table.query(4, 0));
+    ASSERT_EQ(2, table.query(4, 1));
+    ASSERT_EQ(3, table.query(6, 1));
+    ASSERT_EQ(6, table.query(6, tree.depth(6)));
 }
 
 int main (int argc, char **argv) {
