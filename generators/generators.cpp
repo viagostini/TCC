6fd60bffd437c7fbbacfc7bd60b02df59c4c7930
Vinicius Agostini <vinicius.agostini19@gmail.com>
2019-10-21 14:17:04 -0300

    refatoração geradores

--- generators/generators.cpp
+++ generators/generators.cpp
@@ -1,9 +1,4 @@
 #include "generators.hpp"
-#include <iostream>
-#include <cmath>
-
-using std::cout;
-using std::endl;
 
 void create_nodes (int n, vector<Node*>& nodes) {
     for (int i = 0; i < n; i++)
@@ -12,15 +7,16 @@ void create_nodes (int n, vector<Node*>& nodes) {
 
 void build_balanced_kary_tree (int n, int k, vector<Node*>& nodes) {
     create_nodes(n, nodes);
-    int logkn = (int) ceil((log(n) / log(k)));
-    int i;
-    for (i = 0; i < (n-1)/k; i++)
+    for (int i = 0; i < (n-1)/k; i++)
         for (int j = 1; j <= k; j++)
             nodes[i]->add_child(nodes[k*i+j]);
+    int i = (n-1)/k;
     for (int j = k*i+1; j < n; j++)
         nodes[i]->add_child(nodes[j]);
+}
 
-    for (int j = 0; j < n; j++) {
+void check_nodes (vector<Node*>& nodes) {
+    for (int j = 0; j < nodes.size(); j++) {
         cout << "node " << j << endl;
         for (auto child : nodes[j]->children)
             cout << child->label << " ";
--- generators/generators.hpp
+++ generators/generators.hpp
@@ -2,10 +2,15 @@
 #define LINEAR_GEN_H
 
 #include <vector>
+#include <iostream>
+#include <cmath>
 #include "../code/include/Tree.hpp"
 
+using std::cout;
+using std::endl;
 using std::vector;
 
+void check_nodes (vector<Node*>& nodes);
 void create_nodes (int n, vector<Node*>& nodes);
 void build_balanced_kary_tree (int n, int k, vector<Node*>& nodes);
 
